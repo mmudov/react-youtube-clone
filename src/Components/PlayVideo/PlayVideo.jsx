ed2715b153c8f4119a3a7e1e83b08a35df0ec5b8
mmudov <milen.mudov@gmail.com>
2025-02-12 10:34:27 +0200

    Code formatting.

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import './PlayVideo.css'
-import video1 from '../../assets/video.mp4'
+/* import video1 from '../../assets/video.mp4' */
 import like from '../../assets/like.png'
 import dislike from '../../assets/dislike.png'
 import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
-import user_profile from '../../assets/user_profile.jpg'
+/* import user_profile from '../../assets/user_profile.jpg' */
 import { API_KEY, value_converter } from '../../data'
 import moment from 'moment'
 import { useParams } from 'react-router-dom'
 
 const PlayVideo = () => {
     const { videoId } = useParams();
+
     const [apiData, setApiData] = useState(null);
     const [channelData, setChannelData] = useState(null);
     const [commentData, setCommentData] = useState([]);
